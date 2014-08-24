019d0522d8eb0d44e16e2335257244eea84aff7e
FatemaSaifee <fatema.saifee711@github.com>
2014-08-24 09:23:09 +0530

    binary,falsi,secant added

---
+++ README.md.txt
@@ -0,0 +1,2 @@
+Various root findin algorithm from Numerical Analysis of Data for the equation
+	x*x - x - 1
---
+++ secant-method.py
@@ -0,0 +1,25 @@
+def f(x):
+    return (x*x - x - 1);
+
+def searchroots():
+    a=float(input())
+    b=float(input())
+    
+    e = 0.00001
+    i = 0
+    m=(a*f(b)-b*f(a))/(f(b)-f(a))
+    if (f(a) * f(b)) > 0:
+        print "Invalid index"
+    else:
+        while abs(f(m)) > e:
+            
+            m=(a*f(b)-b*f(a))/(f(b)-f(a))
+            i += 1
+            if abs(f(m)) < e:
+                print "root = %3.5f"%m
+            else:
+                print "%3d %3.5f %3.5f %3.5f "%(i,a,b,f(a)*f(m))
+                a=b
+                b=m
+                print "%3.5f %3.5f %3.5f "%(a,b,f(a)*f(m))
+    
